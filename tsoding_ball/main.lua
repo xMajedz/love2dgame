ac6814bd310fc281f65b8918558e2424cd03bfb0
xMajedz <xmajedz@gmail.com>
2025-07-13 10:25:40 +0300

    Update

--- tsoding_ball/main.lua
+++ tsoding_ball/main.lua
@@ -1,11 +1,14 @@
-local w, h = love.graphics.getDimensions()
-local xpos, ypos = w/2, h/2
-local xvel, yvel = 200, 200
-local gravity = 1E3
-local radius = 100
-function love.draw()
-	love.graphics.setBackgroundColor(9/255, 9/255, 9/255, 255/255)
-	local dt = love.timer.getDelta()
+local w, h, xpos, ypos, xvel, yvel, gravity, radius
+
+function love.load()
+	w, h = love.graphics.getDimensions()
+	xpos, ypos = w/2, h/2
+	xvel, yvel = 200, 200
+	gravity = 1E3
+	radius = 100
+end
+
+function love.update(dt)
 	yvel = yvel + gravity * dt
 	local x = xpos + xvel * dt
 	if x + radius > w or x - radius < 0 then
@@ -19,6 +22,10 @@ function love.draw()
 	else
 		ypos = y
 	end
+end
+
+function love.draw()
+	love.graphics.setBackgroundColor(9/255, 9/255, 9/255, 255/255)
 	love.graphics.setColor(230/255, 41/255, 55/255, 255/255)
 	love.graphics.circle("fill", xpos, ypos, radius)
 end
